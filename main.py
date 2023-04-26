7d772836fcc7867b9d4d6859e469df08ed2e9aa6
blakouw <ckamillopx@gmail.com>
2023-04-26 22:00:20 +0200

    o kurde działa ten

--- main.py
+++ main.py
@@ -1,73 +1,92 @@
 import PySimpleGUI as sg
+import numpy as np
 
-# Funkcja obliczająca punktację dla każdej opcji
-def oblicz_punktacje(opcje, kryteria, wagi):
-    punktacja = []
+# Defining GUI layout
+sg.theme('DefaultNoMoreNagging')  # set the color scheme of the GUI
+layout = [
+    [sg.Text('Dodaj magazyn')],
+    [sg.Text('Magazyn:'), sg.InputText(key='magazyn'), sg.Button('Dodaj')],
+    [sg.Text('Dodaj dostawcę')],
+    [sg.Text('Dostawca:'), sg.InputText(key='dostawca'), sg.Button('Dodaj')],
+    [sg.Text('Dodaj koszt')],
+    [sg.Text('Magazyn:'), sg.InputCombo([], key='magazyn_koszt'), sg.Text('Dostawca:'),
+     sg.InputCombo([], key='dostawca_koszt'), sg.Text('Koszt:'), sg.InputText(key='koszt'), sg.Button('Dodaj')],
+    [sg.Text('Tabela kosztów')],
+    [sg.Table(values=[], headings=['Magazyn', 'Dostawca', 'Koszt'], key='tabela_kosztow')],
+    [sg.Button('Oblicz'), sg.Button('Wyjście')],
+    [sg.Text('Tabela zysków jednostkowych')],
+    [sg.Table(values=[], headings=['Magazyn', 'Dostawca', 'Zysk'], key='tabela_zyskow')],
+    [sg.Text('Tabela optymalnych przewozów')],
+    [sg.Table(values=[], headings=['Magazyn', 'Dostawca', 'Przepływ'], key='tabela_przeplywow')],
+    [sg.Text('Koszt całkowity: '), sg.Text(key='koszt_calkowity')],
+    [sg.Text('Przychód całkowity: '), sg.Text(key='przychod_calkowity')],
+    [sg.Text('Zysk pośrednika: '), sg.Text(key='zysk_posrednika')]
+]
 
-    # Stwórz tabelę z wartościami kryteriów dla każdej opcji
-    tablica = [[int(y) for y in x.split(',')] for x in opcje.values()]
+# Defining variables
+magazyny = []
+dostawcy = []
+koszty = []
+zyski = np.array([])
+przeplywy = np.array([])
 
-    # Oblicz sumy ważone dla każdej opcji
-    sumy_wazone = [sum([tablica[i][j] * wagi[j] for j in range(len(kryteria))]) for i in range(len(opcje))]
+# Create the GUI window
+window = sg.Window('Zagadnienie pośrednika', layout)
 
-    # Oblicz sumy wag kryteriów
-    sumy_wag = [sum([wagi[j] for j in range(len(kryteria))]) for i in range(len(opcje))]
-
-    # Oblicz punktację dla każdej opcji
-    for i in range(len(opcje)):
-        s = sum([wagi[j] * tablica[i][j] / sumy_wag[i] for j in range(len(kryteria))])
-        punktacja.append(s)
-
-    # Znajdź najlepszą opcję
-    najlepsza_opcja = list(opcje.keys())[punktacja.index(max(punktacja))]
-
-    # Zwróć najlepszą opcję i jej punktację
-    return najlepsza_opcja, max(punktacja)
-
-# Stwórz interfejs użytkownika
-layout = [[sg.Text("Wprowadź nazwy opcji rozwiązania:")],
-          [sg.Multiline(size=(50, 5), key="-OPCJE-")],
-          [sg.Text("Wprowadź nazwy kryteriów:")],
-          [sg.Multiline(size=(50, 2), key="-KRYTERIA-")],
-          [sg.Text("Wprowadź wartości kryteriów dla każdej opcji:")],
-          [sg.Multiline(size=(50, 5), key="-WARTOSCI-")],
-          [sg.Button("Oblicz"), sg.Button("Wyjdź")]]
-
-window = sg.Window("Zagadnienie pośrednika", layout)
-
-# Rozpocznij pętlę zdarzeń
+# Main event loop
 while True:
     event, values = window.read()
+    if event == 'Dodaj':
+        if values['magazyn']:
+            magazyny.append(values['magazyn'])
+            window['magazyn_koszt'].update(values=magazyny)
+            window['magazyn'].update('')
+        elif values['dostawca']:
+            dostawcy.append(values['dostawca'])
+            window['dostawca_koszt'].update(values=dostawcy)
+            window['dostawca'].update('')
+        elif values['koszt']:
+            koszty.append([values['magazyn_koszt'], values['dostawca_koszt'], float(values['koszt'])])
+            window['tabela_kosztow'].update(values=koszty)
+            window['magazyn_koszt'].update('')
+        elif event == 'Oblicz':
+            if not magazyny or not dostawcy or not koszty:
+                sg.popup('Dodaj magazyny, dostawców oraz koszty')
+            continue
 
-    # Wyjdź z programu, jeśli naciśnięto przycisk "Wyjdź"
-    if event == sg.WIN_CLOSED or event == "Wyjdź":
-        break
+            # Creating the cost matrix
+            cost_matrix = np.zeros((len(magazyny), len(dostawcy)))
+            for koszt in koszty:
+                i = magazyny.index(koszt[0])
+                j = dostawcy.index(koszt[1])
+                cost_matrix[i][j] = koszt[2]
 
-    # Pobierz wprowadzone wartości i podziel na listy
-    opcje = dict(enumerate([x.strip() for x in values["-OPCJE-"].split('\n') if x.strip() != '']))
-    kryteria = [x.strip() for x in values["-KRYTERIA-"].split('\n') if x.strip() != '']
-    wagi = [1.0] * len(kryteria)
-    wartosci = [x.strip() for x in values["-WARTOSCI-"].split('\n') if x.strip() != '']
+            # Solving the transportation problem
+            supply = [1] * len(magazyny)
+            demand = [1] * len(dostawcy)
+            _, przeplywy, zyski = scipy.optimize.linear_sum_assignment(cost_matrix)
 
-    # Sprawdź, czy wprowadzone dane są poprawne
-    if len(opcje) == 0:
-        sg.popup("Wprowadź co najmniej jedną opcję rozwiązania.")
-        continue
+            # Updating tables
+            tabela_zyskow = []
+            tabela_przeplywow = []
+            for i in range(len(magazyny)):
+                for j in range(len(dostawcy)):
+                    if przeplywy[i] == j:
+                        tabela_przeplywow.append([magazyny[i], dostawcy[j], 1])
+                        tabela_zyskow.append([magazyny[i], dostawcy[j], koszty[i * len(dostawcy) + j][2]])
+                    else:
+                        tabela_przeplywow.append([magazyny[i], dostawcy[j], 0])
+                        tabela_zyskow.append([magazyny[i], dostawcy[j], 0])
 
-    if len(kryteria) == 0:
-        sg.popup("Wprowadź co najmniej jedno kryterium.")
-        continue
+            koszt_calkowity = sum([koszt[2] for koszt in koszty])
+            przychod_calkowity = sum(zyski)
+            zysk_posrednika = przychod_calkowity - koszt_calkowity
 
-    # Sprawdź, czy liczba wprowadzonych wartości kryteriów dla każdej opcji jest zgodna z liczbą kryteriów
-    for i in range(len(wartosci)):
-        liczba_wartosci = len(wartosci[i].split(','))
+            window['tabela_zyskow'].update(values=tabela_zyskow)
+            window['tabela_przeplywow'].update(values=tabela_przeplywow)
+            window['koszt_calkowity'].update(koszt_calkowity)
+            window['przychod_calkowity'].update(przychod_calkowity)
+            window['zysk_posrednika'].update(zysk_posrednika)
 
-        if liczba_wartosci != len(kryteria):
-            sg.popup(f"Opcja {i + 1} zawiera niepoprawną liczbę wartości. Wprowadź wartości dla każdego kryterium.")
-
-            # Wyczyść pole z wartościami i przerwij pętlę
-            window["-WARTOSCI-"].update("")
+        elif event in (sg.WIN_CLOSED, 'Wyjście'):
             break
-    # Oblicz punktację i wyświetl wynik
-    najlepsza_opcja, punktacja = oblicz_punktacje(opcje, kryteria, wagi)
-    sg.popup(f"Najlepszą opcją jest: {najlepsza_opcja}\n\nPunktacja: {punktacja:.2f}")
